c0ac66c5a035fe018aa9d3acd31f746ad748d1d8
Anderson-19 <andersonpantor@gmail.com>
2024-07-16 15:47:16 -0400

    Rutas privadas y publicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { privateGuard, publicGuard } from './auth/guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [publicGuard],
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [privateGuard],
+    loadChildren: () => import('./dashboard/dashboard.routes').then((m) => m.routes),
+  },
+];
