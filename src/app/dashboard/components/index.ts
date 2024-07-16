8f4a59fd78007df2fa952089f3fdec559beae202
Anderson-19 <andersonpantor@gmail.com>
2024-07-16 17:07:28 -0400

    Exportación de todos los componentes

---
+++ src/app/dashboard/components/index.ts
@@ -0,0 +1,5 @@
+export * from './contact/contact.component';
+export * from './header-left/header-left.component';
+export * from './header-right/header-right.component';
+export * from './profile/profile.component';
+export * from './other-profile/other-profile.component';
