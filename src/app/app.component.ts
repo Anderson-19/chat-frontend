5e049a80f806bb948ef1c37e91deaf9d4483efe6
Anderson-19 <andersonpantor@gmail.com>
2024-07-16 16:01:12 -0400

    Configuración del app.component para verificar los usuarios autenticados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/services/auth-service';
+import { AuthStatus } from './interfaces';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'chat-frontend';
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public finishedAuthCheck = computed<boolean>(() => {
+    if (this.authService.authStatus() === AuthStatus.checking) {
+      return false;
+    }
+    return true;
+  });
+
+  public authStatusChangedEffect = effect(() => {
+
+    switch (this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
+
+      case AuthStatus.authenticated:
+        this.router.navigateByUrl('dashboard');
+        return;
+
+      case AuthStatus.notAuthenticated:
+        this.router.navigateByUrl('');
+        return;
+    }
+  });
 }
