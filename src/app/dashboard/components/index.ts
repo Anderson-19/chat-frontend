71964d1cbf6d5379f34e61690cd2cb8af546673a
Anderson-19 <andersonpantor@gmail.com>
2024-07-17 15:44:38 -0400

    Exportación del componente message

--- src/app/dashboard/components/index.ts
+++ src/app/dashboard/components/index.ts
@@ -3,3 +3,5 @@ export * from './header-left/header-left.component';
 export * from './header-right/header-right.component';
 export * from './profile/profile.component';
 export * from './other-profile/other-profile.component';
+export * from './message/message.component';
+
